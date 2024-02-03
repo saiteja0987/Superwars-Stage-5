a8ff40733b1a802543f1c3e9903ae7b80cf7df76
Sai Teja <saitejapubglite@gmail.com>
2024-02-03 09:53:13 +0530

    done

--- src/app.js
+++ src/app.js
@@ -33,12 +33,10 @@ class Player {
     this.wins = 0;
   }
 
-  // Get random strength
   getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
   };
 
-  // Create a player for displaying
   view = () => {
     let player = document.createElement('div');
     player.classList.add('player');
@@ -63,7 +61,9 @@ class Superwar {
       let type = i % 2 == 0 ? 'hero' : 'villain';
       return new Player(i, player, type);
     });
-    this.score = [0, 0];
+    this.score = { hero: 0, villain: 0 };
+    this.strength = { hero: 0, villain: 0 };
+    this.wins = { hero: 0, villain: 0 };
     Array.from(document.getElementsByClassName('team')).forEach((elem) =>
       elem.addEventListener('click', (e) => {
         this.handleSelection(e.target);
@@ -110,53 +110,100 @@ class Superwar {
       .forEach((player) => (player.selected = false));
     selectedPlayer.selected = true;
 
-    if (this.isFight() === 'clash') this.fight();
+    if (this.isFight(selectedPlayer.strength) === 'clash') this.fight();
     else this.viewPlayers();
   };
 
   // Progression 1: Check for fight
-  isFight = () => {
+  isFight = (strength) => {
     // Type your code here
-    // return  'clash' or 'peace';
+    if (strength > 0) {
+      return 'clash';
+    } else {
+      return 'peace';
+    }
   };
 
   // Fight
   fight = () => {
-    // Filtered the selected players and calculate score
-    // Should return HTML element with score
-    // Type your code here
+  
+
+    let score = this.calculateScore();
+
+    let finalScore = document.getElementById('score');
+    finalScore.innerHTML = '1 - 0';
 
     if (this.checkWin() !== 'endure')
       setTimeout(() => this.announceWinner(score), 100);
+
+    return finalScore;
   };
 
   // Progression 2: Calculate score
   calculateScore = () => {
-    // Calculate and return the total score of teams
-    // Type your code here
+    let setWinner = this.updateWin();
+    let score = this.score;
+    this.players.map((player) => {
+      if (player.type == 'hero') {
+        player.win = setWinner['hero'];
+        score['hero'] += player.wins;
+      } else {
+        player.win = setWinner['villain'];
+        score['villain'] += player.wins;
+      }
+    });
 
     return score;
   };
 
-  // Progression 3: Check whether there is a win
   checkWin = () => {
-    // Find the winner if exists return type hero or villain
-    // If winner dosen't exists then return endure
-    // Type your code here
+    let result = 'endure';
+    let health = {
+      hero: this.totalStrength('hero'),
+      villain: this.totalStrength('villain'),
+    };
+
+    result =
+      health['hero'] > health['villain']
+        ? 'hero'
+        : health['hero'] == health['villain']
+        ? 'endure'
+        : 'villain';
 
     return result;
   };
 
+  updateWin = () => {
+    let win = { hero: 0, villain: 0, draw: 0 };
+    let winner = this.checkWin();
+    if (winner == 'hero') {
+      win['hero'] += 1;
+    } else if (winner == 'villain') {
+      win['villain'] += 1;
+    } else {
+      win['draw'] += 1;
+    }
+
+    return win;
+  };
+
   // Progression 4: Find total strength of a team
   totalStrength = (type) => {
-    // Calculate and return the total strength of the team
-    // Type your code here
+    let strength = this.strength;
+    this.players.map((player) => {
+      if (player.type == 'hero') {
+        strength['hero'] += player.strength;
+      } else {
+        strength['villain'] += player.strength;
+      }
+    });
 
-    return strength;
+    return strength[type];
   };
 
   // Announce the winner
   announceWinner = (score) => {
+    console.log(score['hero']);
     if (score['hero'] == score['villain']) alert('Its a draw!');
     else if (score['hero'] > score['villain']) alert('Heroes Win!');
     else alert('Villains Win!');
